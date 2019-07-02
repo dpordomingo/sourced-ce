d8e0e73038ebe8b4ed0971eecc64fb5893ee12b6
David Pordomingo <David.Pordomingo.F@gmail.com>
2019-07-02 17:23:04 +0200

    wip
    Signed-off-by: David Pordomingo <David.Pordomingo.F@gmail.com>

--- cmd/sourced/cmd/prune.go
+++ cmd/sourced/cmd/prune.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/src-d/sourced-ce/cmd/sourced/compose"
 	"github.com/src-d/sourced-ce/cmd/sourced/compose/workdir"
@@ -10,21 +11,39 @@ import (
 type pruneCmd struct {
 	Command `name:"prune" short-description:"Stop and remove components and resources" long-description:"Stops containers and removes containers, networks, volumes and configuration created by 'init' for the current working directory.\nTo delete resources for all working directories pass --all flag.\nImages are not deleted unless you specify the --images flag."`
 
-	All    bool `short:"a" long:"all" description:"Remove containers and resources for all working directories"`
+	All    bool `short:"a" long:"all" description:"Remove containers and resources for all working directories. It will be ignored if a list of workdirs is passed."`
 	Images bool `long:"images" description:"Remove docker images"`
+	Args   struct {
+		Workdirs []string `positional-arg-name:"workdir" description:"Workdir to be pruned"`
+	} `positional-args:"yes"`
 }
 
 func (c *pruneCmd) Execute(args []string) error {
-	if !c.All {
+	current, _ := workdir.Active()
+	var dirs []string
+	var err error
+	if len(c.Args.Workdirs) > 0 {
+		for _, dir := range c.Args.Workdirs {
+			err = workdir.ValidatePath(dir)
+			if err != nil {
+				fmt.Printf("ignored workdir '%s': %s\n", dir, err)
+				continue
+			}
+
+			dirs = append(dirs, dir)
+		}
+	} else if c.All {
+		dirs, err = workdir.ListPaths()
+		if err != nil {
+			return err
+		}
+	} else {
 		return c.pruneActive()
 	}
 
-	dirs, err := workdir.ListPaths()
-	if err != nil {
-		return err
-	}
-
 	for _, dir := range dirs {
+		fmt.Println(dir)
+		continue
 		if err := workdir.SetActivePath(dir); err != nil {
 			return err
 		}
@@ -32,6 +51,15 @@ func (c *pruneCmd) Execute(args []string) error {
 		if err = c.pruneActive(); err != nil {
 			return err
 		}
+
+		if current == dir {
+			current = ""
+		}
+	}
+
+	return nil
+	if current != "" {
+		workdir.SetActivePath(current)
 	}
 
 	return nil
